25e6aa91bfae82c352e2fe1e44765f1b1ae9c1a4
CailieAC <cailiecarlile@gmail.com>
2019-03-29 23:28:11 -0500

    comment description

--- ClassEnum/Program.cs
+++ ClassEnum/Program.cs
@@ -4,13 +4,25 @@ namespace ClassEnum
 {
     public static class Program
     {
+        //Complete the ClassEnum program with an enumeration of the first names of all the students. 
+        //Ask the user to enter their name. Using a Switch statement, print their last name. 
+        //Add at least two students, one named Mickey Mouse and another named Donald Duck.
+
+        enum FirstNames
+        {
+
+        }
+
         public static void Main()
         {
-            Console.ReadLine();
+            Console.Write("Enter your name: ");
+            string result = GetFirstName(StudentType.Parse((Console.ReadLine()));
+            Console.WriteLine()));
         }
 
         public static string GetFirstName(StudentType student)
         {
+
         }
     }
 
